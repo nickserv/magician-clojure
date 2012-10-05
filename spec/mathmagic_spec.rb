df6e41cbfe92d4faef62a7e449ed507512c368d2
Nicolas McCurdy <thenickperson@gmail.com>
2012-10-05 19:49:50 -0400

    Add initial rspec tests

--- spec/mathmagic_spec.rb
+++ spec/mathmagic_spec.rb
@@ -1,7 +1,13 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Mathmagic" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
-  end
+describe 'Mathmagic' do
+
+	it 'should get the sum of some integers' do
+		[1,2,3].sum.should == 6
+	end
+
+	it 'should get the mean of some integers' do
+		[1,2,3].mean.should == 2.0
+	end
+
 end
