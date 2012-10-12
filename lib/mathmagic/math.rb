4233d3a3f9d02af862c6fb7158c3e2015e9cb6be
Nicolas McCurdy <thenickperson@gmail.com>
2012-10-12 04:26:19 -0400

    Add Math module additions

---
+++ lib/mathmagic/math.rb
@@ -0,0 +1,22 @@
+module Math
+
+	def permutations(n, k)
+		return n.factorial / (n-k).factorial
+	end
+
+	def combinations(n, k)
+		return n.factorial / (k.factorial * (n-k).factorial)
+	end
+
+	def collatz(n, depth=0)
+		depth += 1
+		if n == 1
+			return depth
+		elsif n % 2 == 0
+			return collatz(n/2, depth)
+		else
+			return collatz(3*n + 1, depth)
+		end
+	end
+
+end
