cf4e8b651e1bef5d56deca56ca2c7038be7870fd
Nicolas McCurdy <thenickperson@gmail.com>
2012-11-27 00:58:49 -0500

    Add Integer#get_factors

--- lib/mathmagic/integer.rb
+++ lib/mathmagic/integer.rb
@@ -1,5 +1,24 @@
 class Integer
 
+	def factors
+		#first half
+		factors = []
+		for i in 1..self
+			if self % i == 0
+				if i < self/i
+					factors << i
+				else break
+				end
+			end
+		end
+		#second half
+		factors_old = factors.dup
+		until factors_old.length == 0
+			factors << self/factors_old.pop
+		end
+		return factors
+	end
+
 	def factorial
 		(1..self).inject(:*)
 	end
