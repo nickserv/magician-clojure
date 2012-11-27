6ce2d6aa07e2c8c9b394e03430166c3e0232cb60
Nicolas McCurdy <thenickperson@gmail.com>
2012-11-27 16:53:12 -0500

    Require everything in lib/magician automatically

--- lib/magician.rb
+++ lib/magician.rb
@@ -1,8 +1,3 @@
 # Require all of magician's stuff
-lib_path = File.join(File.dirname(__FILE__), 'magician')
-require File.join(lib_path, 'array')
-require File.join(lib_path, 'integer')
-require File.join(lib_path, 'string')
-require File.join(lib_path, 'math')
-require File.join(lib_path, 'numeric')
-require File.join(lib_path, 'shortcuts')
+lib_path = File.join(File.dirname(__FILE__), 'magician', '*.rb')
+Dir[lib_path].each { |file| require file }
