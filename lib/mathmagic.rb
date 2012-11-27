c6c39820458cba53ec7ce11fc35a163261c325ee
Nicolas McCurdy <thenickperson@gmail.com>
2012-11-26 20:31:36 -0500

    Require the Math class (oopsy, forgot this earlier...)

--- lib/mathmagic.rb
+++ lib/mathmagic.rb
@@ -2,4 +2,5 @@
 lib_path = File.join(File.dirname(__FILE__), 'mathmagic')
 require File.join(lib_path, 'array')
 require File.join(lib_path, 'integer')
+require File.join(lib_path, 'math')
 require File.join(lib_path, 'shortcuts')
