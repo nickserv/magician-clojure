e9ee4c9d29ad340dd2dd7a7c1cf77d50d326e93c
Nicolas McCurdy <thenickperson@gmail.com>
2012-11-28 17:14:12 -0500

    Write some more tests for Math

--- spec/math_spec.rb
+++ spec/math_spec.rb
@@ -2,12 +2,56 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe Math do
 
-	it 'should solve a quadratic formula'
-	it 'should get the number of permutations with n and k'
-	it 'should get the number of combinations with n and k'
-	it 'should get the number of steps to finish the Collatz Conjecture'
+	it 'should solve a quadratic formula' do
+		Math.quadratic(1, 2, 3).should == 'change me'
+		Math.quadratic(-1, -2, -3).should == 'change me'
+		Math.quadratic(1, 1, 1).should == 'change me'
+		Math.quadratic(1, 0, 0).should == 0
+		Math.quadratic(0, 1, 2).should == nil
+	end
+
+	it 'should get the number of permutations with n and k' do
+		Math.permutations(10, 5).should == 30_240
+		Math.permutations(5, 10).should == nil
+		Math.permutations(5, 5).should == 120
+		Math.permutations(5, 0).should == 0
+		Math.permutations(0, 5).should == nil
+		Math.permutations(0, 0).should == 0
+		Math.permutations(-5, 5).should == nil
+		Math.permutations(5, -5).should == nil
+		Math.permutations(-5, -5).should == nil
+	end
+
+	it 'should get the number of combinations with n and k' do
+		Math.permutations(10, 5).should == 30_240
+		Math.permutations(5, 10).should == nil
+		Math.permutations(5, 5).should == 120
+		Math.permutations(5, 0).should == 0
+		Math.permutations(0, 5).should == nil
+		Math.permutations(-5, 5).should == nil
+		Math.permutations(5, -5).should == nil
+		Math.permutations(-5, -5).should == nil
+	end
+
+	it 'should get the number of steps to finish the Collatz Conjecture' do
+		Math.collatz(-1).should == nil
+		Math.collatz(0).should == nil
+		Math.collatz(1).should == 0
+		Math.collatz(2).should == 1
+		Math.collatz(7).should == 16
+		Math.collatz(100).should == 25
+	end
+
 	it 'should get the length of a hypotenuse with the Pythagorean theorem'
 	it 'should determine if three given numbers form a Pythagorean triplet'
-	it 'should calculate a series of Fibonacci numbers of a specified length'
+
+	it 'should calculate a series of Fibonacci numbers of a specified length' do
+		Math.fibs(-1).should == nil
+		Math.fibs(0).should == []
+		Math.fibs(1).should == [1]
+		Math.fibs(2).should == [1, 1]
+		Math.fibs(5).should == [1, 1, 2, 3, 5]
+		Math.fibs(10).should == [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+	end
 
 end
