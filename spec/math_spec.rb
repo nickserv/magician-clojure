0644e6d8dddcf982362e131acc9d023b333985df
Nicolas McCurdy <thenickperson@gmail.com>
2012-11-28 02:08:55 -0500

    Add Math spec with a few pending tests

---
+++ spec/math_spec.rb
@@ -0,0 +1,13 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+describe Math do
+
+	it 'should solve a quadratic formula'
+	it 'should get the number of permutations with n and k'
+	it 'should get the number of combinations with n and k'
+	it 'should get the number of steps to finish the Collatz Conjecture'
+	it 'should get the length of a hypotenuse with the Pythagorean theorem'
+	it 'should determine if three given numbers form a Pythagorean triplet'
+	it 'should calculate a series of Fibonacci numbers of a specified length'
+
+end
