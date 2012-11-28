6fd60f5bf93a08c5760287e8782c60add0ee459d
Nicolas McCurdy <thenickperson@gmail.com>
2012-11-28 02:59:31 -0500

    Fix Array methods for empty Arrays and update their docs accordingly

--- lib/magician/array.rb
+++ lib/magician/array.rb
@@ -1,40 +1,48 @@
 # Magician's extensions to the Array class.
 class Array
 
-	# Gets the sum of the numbers in the array.
+	# Gets the sum of the numbers in the array. The sum of an empty array is 0.
 	#
 	# @return [Numeric] the sum of the numbers in the array
 	def sum
+		return 0 if self.empty?
 		inject(:+)
 	end
 
-	# Gets the product of the numbers in the array.
+	# Gets the product of the numbers in the array. The product of an empty array
+	# is 1.
 	#
 	# @return [Numeric] the product of the numbers in the array
-	def multiply
+	def product
+		return 1 if self.empty?
 		inject(:*)
 	end
 
-	# Gets the range of the numbers in the array (maximum - minimum).
+	# Gets the range of the numbers in the array (maximum - minimum). The range of
+	# an empty array is nil.
 	#
 	# @return [Numeric] the range of the numbers in the array
 	def range
+		return nil if self.empty?
 		max - min
 	end
 
-	# Gets the mean (average) of the numbers in the array.
+	# Gets the mean (average) of the numbers in the array. The mean of an empty
+	# array is nil.
 	#
 	# @return [Float] the mean (average) of the numbers in the array
 	def mean
+		return nil if self.empty?
 		sum.to_f / size
 	end
 
 	# Gets the median of the numbers in the array (the value in the middle of a
 	# sorted version of the array). If the array has an even length, the middle
-	# two values are averaged.
+	# two values are averaged. The median of an empty array is nil.
 	#
 	# @return [Numeric] the median of the numbers in the array
 	def median
+		return nil if self.empty?
 		sorted = sort
 		if sorted.length.odd?
 			self[length/2]
@@ -44,11 +52,12 @@ class Array
 	end
 
 	# Gets the mode(s) of the items in the array (the item(s) that occur(s) most
-	# frequently).
+	# frequently). The mode of an empty array is nil.
 	#
 	# @return [Array] an array of all of the items in the array that occur the
 	# most freqeuntly (they must all have the same number of occurences)
 	def mode
+		return nil if self.empty?
 		occ = occurences
 		max_occ = occ.values.max
 		occ.select { |key, value| value == max_occ }.keys
