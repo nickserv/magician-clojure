fe45ef65f856dbc15a4cf9e1e788bd08fe87b352
Nicolas McCurdy <thenickperson@gmail.com>
2012-11-28 02:00:03 -0500

    Add an additional doc note to a Numeric#digits

--- lib/magician/numeric.rb
+++ lib/magician/numeric.rb
@@ -1,7 +1,8 @@
 # Magician's extensions to the Numeric class (affects Integers and Floats).
 class Numeric
 
-	# Performs self.to_s[selection].to_i on the number.
+	# Performs self.to_s[selection].to_i on the number. Note that for floats, the
+	# decimal counts as a digit within the string.
 	# TODO: Let this intelligently convert back to an Integer or Float.
 	#
 	# @param [Range] selection the selection/range to get from the number (you can
