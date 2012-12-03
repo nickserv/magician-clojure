a1f16fe4d9bce751245113bf62096bfbba9ea2d0
Nicolas McCurdy <thenickperson@gmail.com>
2012-12-02 21:47:20 -0500

    In Array spec, test for Arrays that contain strings

--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -5,6 +5,7 @@ describe Array do
 	it 'should calculate its sum' do
 		[].sum.should == 0
 		[1].sum.should == 1
+		['string', 1].sum.should == 1
 		[1, 2, 3, 4].sum.should == 10
 		[-4, 0, 5].sum.should == 1
 	end
@@ -12,18 +13,21 @@ describe Array do
 	it 'should calculate its product' do
 		[].product.should == 1
 		[1].product.should == 1
+		['string', 1].product.should == 1
 		[5, 7, 2].product.should == 70
 	end
 
 	it 'should calculate its range' do
 		[].range.should == nil
 		[4].range.should == 0
+		['string', 4].range.should == 0
 		[5, 1, 10].range.should == 9
 	end
 
 	it 'should calculate its mean' do
 		[].mean.should == nil
 		[4].mean.should == 4
+		['string', 4].mean.should == 4
 		[-3, 0, 6].mean.should == 1
 		[1, 2, 3, 4, 5].mean.should == 3
 	end
@@ -31,6 +35,7 @@ describe Array do
 	it 'should calculate its median' do
 		[].median.should == nil
 		[4].median.should == 4.0
+		['string', 4].median.should == 4.0
 		[2, 1, 3, 5, 4].median.should == 3.0
 		[1, 2, 3, 4].median.should == 2.5
 	end
@@ -38,6 +43,7 @@ describe Array do
 	it 'should calculate its mode' do
 		[].mode.should == nil
 		[4].mode.should == [4]
+		['string', 4].mode.should == [4]
 		[1, 2, 1, 3, 1, 4].mode.should == [1]
 		[1, 1, 1, 2, 2, 2, 3].mode.should == [1, 2]
 	end
@@ -45,6 +51,7 @@ describe Array do
 	it 'should calculate a hash holding numbers of occurrences of its items' do
 		[].occurences.should == {}
 		[4].occurences.should == { 4=>1 }
+		['string', 4].occurences.should == { 'string'=>1, 4=>1 }
 		[1, 2, 2, 5].occurences.should == { 1=>1, 2=>2, 5=>1 }
 	end
 
