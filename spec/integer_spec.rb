a556460996ba47cd06d17342c1a69faca3ceb90f
Nicolas McCurdy <thenickperson@gmail.com>
2013-03-08 02:51:36 -0500

    Add tests for Integer#pandigital?

--- spec/integer_spec.rb
+++ spec/integer_spec.rb
@@ -32,6 +32,14 @@ describe Integer do
     -6.prime?.should be_false
   end
 
-  it 'should determine if it is pandigital'
+  it 'should determine if it is pandigital' do
+    123456789.pandigital?.should be_true
+    987654321.pandigital?.should be_true
+    192837465.pandigital?.should be_true
+    12345.pandigital?.should be_false
+    1234567890.pandigital?.should be_false
+    1234567899.pandigital?.should be_false
+    112233445566778899.pandigital?.should be_false
+  end
 
 end
