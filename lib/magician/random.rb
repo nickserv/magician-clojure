88d2c32f81680f2a9f6cb99f3d3512e5c04d5741
Nicolas McCurdy <thenickperson@gmail.com>
2013-03-13 03:35:12 -0400

    Finish docs for Random

--- lib/magician/random.rb
+++ lib/magician/random.rb
@@ -7,14 +7,23 @@ class Random
     define_singleton_method(name) { DEFAULT.send name }
   end
 
+  # Returns a random boolean (true or false).
+  #
+  # @return [Boolean] true or false
   def boolean
     [true, false].sample
   end
 
+  # Returns a random coin toss (heads or tails).
+  #
+  # @return [String] 'heads' or 'tails'
   def coin
     ['heads', 'tails'].sample
   end
 
+  # Returns a random die roll (from 1-6).
+  #
+  # @return [Fixnum] a Fixnum from 1-6
   def die
     rand 1..6
   end
