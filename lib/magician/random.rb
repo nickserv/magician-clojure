08a1215b887cd046c7552ee98a12f4b2989d9bff
Nicolas McCurdy <thenickperson@gmail.com>
2013-03-13 03:31:30 -0400

    Syntax error fix

--- lib/magician/random.rb
+++ lib/magician/random.rb
@@ -4,7 +4,7 @@ class Random
   # For all the instance methods defined below, add a class method which invokes
   # the instance method on DEFAULT.
   [:boolean, :coin, :die].each do |name|
-    define_singleton_method name { DEFAULT.send name }
+    define_singleton_method(name) { DEFAULT.send name }
   end
 
   def boolean
