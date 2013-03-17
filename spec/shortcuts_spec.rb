c9c78d880609c8bc895e59552af8d8a9f800abef
Nicolas McCurdy <thenickperson@gmail.com>
2013-03-17 04:13:47 -0400

    Update the Shortcuts spec

--- spec/shortcuts_spec.rb
+++ spec/shortcuts_spec.rb
@@ -5,6 +5,8 @@ describe 'Shortcuts' do
   it 'should properly alias certain values from Math' do
     PI.should == Math::PI
     E.should == Math::E
+    I.should == Complex::I
+    GOLDEN_RATIO.should == (1 + Math.sqrt(5))/2
   end
 
 end
