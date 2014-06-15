12f045453741fccd9dac6c88fc3c18ae7db1fc26
Nicolas McCurdy <thenickperson@gmail.com>
2014-06-15 04:15:33 -0400

    Port numeric.lisp to Clojure

--- src/magician/numeric.clj
+++ src/magician/numeric.clj
@@ -1,20 +1,23 @@
+(ns magician.numeric)
+
 ;;;;  Magician's extensions to the Numeric class (affects Integers and Floats).
 
-(in-package #:magician)
-
-(defun is-divisible-p (x y)
+(defn divisible?
   "Returns true if for numbers x and y, x is evenly divisible by y.
 
   If y is equal to 0, it returns false, since numbers cannot be divided by 0 in
   real number arithmetic."
+  [x y]
   (if (= y 0)
-    nil
-    (= 0 (mod x y))))
+    false
+    (zero? (mod x y))))
 
-(defun degrees->radians (n)
+(defn degrees->radians
   "Converts a number from degrees to radians."
-  (* n (/ pi 180)))
+  [n]
+  (* n (/ Math/PI 180)))
 
-(defun radians->degrees (n)
+(defn radians->degrees
   "Converts a number from radians to degrees."
-  (* n (/ 180 pi)))
+  [n]
+  (* n (/ 180 Math/PI)))
---
+++ test/magician/numeric_test.clj
@@ -0,0 +1,34 @@
+(ns magician.numeric-test
+  (:require [clojure.test :refer :all]
+            [magician.numeric :refer :all]))
+
+(deftest divisible?-test
+      "should determine if it is divisible by another number"
+      (is (divisible? 0 5))
+      (is (not (divisible? 1 6)))
+      (is (divisible? -1 1))
+      (is (divisible? 12 6))
+      (is (not (divisible? 6 5)))
+      (is (not (divisible? 10 0)))
+      (is (divisible? 9 1.5))
+      (is (divisible? 9.0 1.5))
+      (is (divisible? 10.5 1.5))
+      (is (not (divisible? 10.5 1))))
+
+(deftest degrees->radians-test
+      "should convert angles to radians"
+      (is (= 0.0 (degrees->radians 0)))
+      (is (= (/ Math/PI 2) (degrees->radians 90)))
+      (is (= Math/PI (degrees->radians 180)))
+      (is (= (* (/ 3 2) Math/PI) (degrees->radians 270)))
+      (is (= (* 2 Math/PI) (degrees->radians 360)))
+      (is (= (- (/ Math/PI 2)) (degrees->radians -90))))
+
+(deftest radians->degrees-test
+      "should convert angles to degrees"
+      (is (= 0.0 (radians->degrees 0)))
+      (is (= 90.0 (radians->degrees (/ Math/PI 2))))
+      (is (= 180.0 (radians->degrees Math/PI)))
+      (is (= 270.0 (radians->degrees (* (/ 3 2) Math/PI))))
+      (is (= 360.0 (radians->degrees (* 2 Math/PI))))
+      (is (= -90.0 (radians->degrees (- (/ Math/PI 2))))))
