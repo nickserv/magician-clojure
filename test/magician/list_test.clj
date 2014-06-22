b4a541f83e73afb47d4f6bd39c3fdd015e0ee1ae
Nicolas McCurdy <thenickperson@gmail.com>
2014-06-22 14:42:14 -0400

    Test with lists instead of vectors

--- test/magician/list_test.clj
+++ test/magician/list_test.clj
@@ -3,7 +3,7 @@
             [magician.list :refer :all]))
 
 (deftest numerics-test
-  (is (= [1 2.0 -3] (numerics [1 2.0 -3])))
-  (is (= [] (numerics ["string"])))
-  (is (= [] (numerics [])))
-  (is (= [1 3] (numerics [1, "two", 3, "four"]))))
+  (is (= '(1 2.0 -3) (numerics '(1 2.0 -3))))
+  (is (= () (numerics '("string"))))
+  (is (= () (numerics ())))
+  (is (= '(1 3) (numerics '(1 "two" 3 "four")))))
